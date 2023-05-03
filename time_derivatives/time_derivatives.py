068a5dae8ace406e088dbd3dc753b31abcf52e25
Ingrid Kristine Jacobsen <ingrid.kristine.jacobsen@gmail.com>
2023-05-03 10:02:47 +0200

    Found bug with inertia term

--- time_derivatives/time_derivatives.py
+++ time_derivatives/time_derivatives.py
@@ -1,5 +1,4 @@
 import porepy as pp
-from porepy.numerics.ad.time_derivatives import time_increment
 
 
 def inertia_term(
@@ -9,13 +8,20 @@ def inertia_term(
     ddt_op: pp.ad.Operator,
     time_step: pp.ad.Scalar,
 ) -> pp.ad.Operator:
-    """This one is probably fine. Keep it as an operator"""
+    """Operator for the inertial term in the dynamic balance equation.
+
+    Parameters:
+        model: Model that is run. Needed for accessing the beta-constant.
+        op:
+        dt_op: First time derivative of op operator.
+        ddt_op: Second time derivative of op operator.
+    """
     beta = model.beta
 
     increment = (
         op
-        - op.previous_timestep
+        - op.previous_timestep()
         - time_step * dt_op.previous_timestep()
-        - (1 - 2 * beta) * time_step * time_step / 2 * ddt_op.previous_timestep()
+        - time_step * time_step * (1 - 2 * beta) * ddt_op.previous_timestep()
     )
     return increment / (time_step * time_step)
