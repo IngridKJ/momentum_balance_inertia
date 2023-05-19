Sign bug in source term + two analytical solutions

2D_dynamic_time_dep_source.py
@@ -60,10 +60,16 @@ class BoundaryAndInitialCond:
         y = sd.cell_centers[1, :]
 
         vals = np.zeros((self.nd, sd.num_cells))
-
-        vals[0] = 2 * x * y * (1 - x) * (1 - y)
-        vals[1] = 2 * x * y * (1 - x) * (1 - y)
-        return vals.ravel("F")
+        manufactured_sol = self.params.get("manufactured_solution", "bubble")
+        if manufactured_sol == "bubble":
+            vals[0] = 2 * x * y * (1 - x) * (1 - y)
+            vals[1] = 2 * x * y * (1 - x) * (1 - y)
+        elif manufactured_sol == "quad_time":
+            vals[0] = 2 * np.sin(np.pi * x) * np.sin(np.pi * y)
+            vals[1] = 2 * np.sin(np.pi * x) * np.sin(np.pi * y)
+        elif manufactured_sol == "quad_space":
+            raise NotImplementedError
+        return vals.ravel("F") * 0
 
 
 class Source:
@@ -120,12 +126,13 @@ class MyMomentumBalance(
 
 
 time_manager = pp.TimeManager(
-    schedule=[0, 1e-2],
-    dt_init=1e-4,
+    schedule=[0, 1e0],
+    dt_init=1e-2,
     constant_dt=True,
     iter_max=10,
     print_info=True,
 )
+
 
 solid_constants = pp.SolidConstants(
     {
@@ -141,7 +148,8 @@ material_constants = {"solid": solid_constants}
 params = {
     "time_manager": time_manager,
     "material_constants": material_constants,
-    "folder_name": "nothing_to_care_about_viz",
+    "folder_name": "test_convergence_viz",
+    "manufactured_solution": "bubble",
 }
 model = MyMomentumBalance(params)
 

utils/utility_functions.py
@@ -1,3 +1,12 @@
+"""
+ParaView quad_time analytical solution:
+(time)^2 * (sin(acos(-1) * coords[0]) * sin(acos(-1) * coords[1]) * iHat + sin(acos(-1) * coords[0]) * sin(acos(-1) * coords[1]) * jHat)
+
+ParaView bubble analytical solution:
+(time)^2 * (coords[0] * coords[1] * (1 - coords[0]) * (1 - coords[1]) * iHat + coords[0] * coords[1] * (1 - coords[0]) * (1 - coords[1]) * jHat)
+
+"""
+
 import numpy as np
 import porepy as pp
 from typing import Optional
@@ -131,9 +140,16 @@ def body_force_func_time(model) -> list:
 
     x, y, t = sym.symbols("x y t")
 
-    # Manufactured solution (bubble<3)
-    u1 = u2 = t**2 * x * (1 - x) * y * (1 - y)
-    u = [u1, u2]
+    manufactured_sol = model.params.get("manufactured_solution", "bubble")
+    if manufactured_sol == "bubble":
+        # Manufactured solution (bubble<3)
+        u1 = u2 = t**2 * x * (1 - x) * y * (1 - y)
+        u = [u1, u2]
+    elif manufactured_sol == "quad_time":
+        u1 = u2 = t**2 * sym.sin(np.pi * x) * sym.sin(np.pi * y)
+        u = [u1, u2]
+    elif manufactured_sol == "quad_space":
+        raise NotImplementedError
 
     ddt_u = [
         sym.diff(sym.diff(u[0], t), t),
@@ -171,8 +187,8 @@ def body_force_func_time(model) -> list:
     acceleration_term = [rho * ddt_u[0], rho * ddt_u[1]]
 
     full_eqn = [
-        acceleration_term[0] + div_sigma[0],
-        acceleration_term[1] + div_sigma[1],
+        acceleration_term[0] - div_sigma[0],
+        acceleration_term[1] - div_sigma[1],
     ]
 
     return [