e74278ff4d7982c56c07355bfb735252721acef4
Ingrid Kristine Jacobsen <ingrid.kristine.jacobsen@gmail.com>
2023-11-23 09:48:05 +0100

    MISC: Unit-test-relevant files

--- run_simulations/spatial_convergence_unit.py
+++ run_simulations/spatial_convergence_unit.py
@@ -22,9 +22,9 @@ def fetch_numbers_from_file(filename) -> np.ndarray:
 
 def plot_the_error(filename, write_stats: bool = True, color: tuple = (0, 0, 0)):
     """Plots the error values fetched from a text file and computes max/min/average."""
-    numbers = fetch_numbers_from_file(filename)[50:]
+    numbers = fetch_numbers_from_file(filename)[500:]
 
-    start_from = 50
+    start_from = 500
     time_steps = np.linspace(1, len(numbers), len(numbers))
 
     if write_stats:
@@ -42,25 +42,21 @@ def plot_the_error(filename, write_stats: bool = True, color: tuple = (0, 0, 0))
     plt.title(filename)
 
 
+plt.show()
+
 errors = np.array(
     [
-        0.09911769096837494,
-        0.052971377181679664,
-        0.0275207821680987,
-        0.015761422023380052,
-        0.010423886548409166,
+        0.21318820784866074,
+        0.07610938562077103,
+        0.01931304141697523,
+        0.006837330341689268,
+        0.0028005276194816233,
+        0.001719848260489023,
     ]
 )
 
-cell_sizes = np.array(
-    [
-        0.2,
-        0.1,
-        0.05,
-        0.025,
-        0.0125,
-    ]
-)
+cell_sizes = np.array([2.0, 1.0, 0.5, 0.25, 0.125, 0.0625])
+
 
 plt.loglog(
     cell_sizes,
@@ -69,16 +65,21 @@ plt.loglog(
     color=pu.RGB(157 - 20, 77 + 40, 159 + 20),
     label="Displacement",
 )
-
+plt.ylabel("Relative l2 error", fontsize=13)
 plt.loglog(
-    cell_sizes[:-3],
-    0.15 * cell_sizes[:-3],
+    cell_sizes[:-4],
+    0.0015 * cell_sizes[:-4],
     color=pu.RGB(157 - 20, 77 + 70, 159 + 20),
     label="Slope 1 reference line",
 )
-plt.ylabel("Relative l2 error", fontsize=13)
-plt.xlabel("Time-step size [s]", fontsize=13)
-plt.title("Temporal convergence unit test", fontsize=13)
+plt.loglog(
+    cell_sizes[:-4],
+    0.005 * cell_sizes[:-4] ** 2,
+    color=pu.RGB(200, 77 + 70, 159 + 60),
+    label="Slope 2 reference line",
+)
+plt.xlabel("Cell size [m]", fontsize=13)
+plt.title("Spatial convergence unit test", fontsize=13)
 plt.grid(True, which="both", color=(0.87, 0.87, 0.87))
 plt.legend(fontsize=12)
 plt.show()
