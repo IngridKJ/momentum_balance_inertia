54bc6bc957a7afdcba96c741bc9e1d785b11a2ba
Ingrid Kristine Jacobsen <ingrid.kristine.jacobsen@gmail.com>
2023-11-23 11:03:14 +0100

    MISC: Improved plotting function

--- plotting/plot_utils.py
+++ plotting/plot_utils.py
@@ -31,27 +31,35 @@ def read_float_values(filename) -> np.ndarray:
 
 def create_and_save_animation(
     file_path: str,
+    main_color: tuple,
     additional_lines_info: list[tuple],
+    y_ax: tuple,
+    logarithmic: bool = True,
     output_file: str = None,
     save_as_mp4: bool = False,
     duration_seconds: int = None,
     framerate: int = None,
+    relative: bool = True,
 ) -> None:
     """Function for making animation from values from a text file.
 
     Parameters:
         file_path: These are the lead characters of the plot. These values will have a
             dot following them as time passes.
+        main_color: Tuple representing RGB colors for the main line.
         additional_lines_info: Additional files' values to be plotted. They will not
             have any animation and are thus just lineplots. It is a list consisting of
             tuples. The tuples must contain:
                 * a string file path
                 * a tuple with three RGB-values
                 * a string with the line label name
+        y_ax: Tuple with minimum and maximum value of the y-axis.
+        logarithmic: Whether the y-axis should be plotted with log scale.
         output_file: Name of the .mp4 that might be saved.
         duration_seconds: Duration of the .mp4 (not sure if this actually works)
         framerate: framerate of the .mp4
         save_as_mp4: Whether an .mp4 should be saved or not.
+        relative: Whether the values should be relative to the first energy value.
 
     Example:
         create_and_save_animation(
@@ -67,33 +75,83 @@ def create_and_save_animation(
     """
     # Read float values from the main file
     float_values = read_float_values(file_path)
+    if relative:
+        float_values = float_values / max(float_values)
 
-    # Extract information for additional lines
-    additional_lines = [
-        (read_float_values(file), RGB(red, green, blue), label)
-        for file, (red, green, blue), label in additional_lines_info
-    ]
+        # Extract information for additional lines
+        additional_lines = [
+            (
+                (read_float_values(file)) / max(read_float_values(file)),
+                RGB(red, green, blue),
+                label,
+            )
+            for file, (red, green, blue), label in additional_lines_info
+        ]
+    else:
+        # Extract information for additional lines
+        additional_lines = [
+            (read_float_values(file), RGB(red, green, blue), label)
+            for file, (red, green, blue), label in additional_lines_info
+        ]
 
     # Set up the plot
     fig, ax = plt.subplots()
     ax.grid(True, which="both", color=(0.87, 0.87, 0.87))
-    ax.set_yscale("log")
-    ax.set_xlim(0, len(float_values))
-    ax.set_ylim(0, 50000)
-    ax.set_xlabel("Frame")
-    ax.set_ylabel("Float Value")
+
+    if logarithmic:
+        ax.set_yscale("log")
+
+    ax.set_xlim(0, len(float_values) + 10)
+    ax.set_ylim(y_ax[0], y_ax[1])
+    ax.set_xlabel("Time steps")
+    ax.set_ylabel("Energy")
+    ax.set_title("Energy evolution of a rotated wave")
 
     # Create additional persistent lines
+    # First persistent vertical dashed lines:
+    xmin = 10 / np.sqrt(3) * 100
+    xmax = np.sqrt(200) / np.sqrt(3) * 100
+
+    ax.axhline(
+        y=0,
+        xmin=0,
+        xmax=12,
+        color=(0, 0, 0),
+        linewidth=0.5,
+    )
+
+    ax.axvline(
+        x=xmin,
+        ymin=0,
+        ymax=5,
+        color=(0.65, 0.65, 0.65),
+        linestyle="--",
+        linewidth=1,
+    )
+    ax.axvline(
+        x=xmax,
+        ymin=0,
+        ymax=5,
+        linestyle="--",
+        color=(0.65, 0.65, 0.65),
+        linewidth=1,
+    )
+
+    # Then the other energy values:
     additional_line_objects = [
-        ax.plot([], [], label=label, color=color)[0]
+        ax.plot([], [], "--", label=label, color=color)[0]
         for _, color, label in additional_lines
     ]
 
     # Create the initial red dashed line
-    (red_line,) = ax.plot([], [], "r", label="Energy")
+    (red_line,) = ax.plot(
+        [], [], color=RGB(main_color[0], main_color[1], main_color[2]), label="Energy"
+    )
 
     # Create the dynamic red dot
-    (red_dot,) = ax.plot([], [], "ro")
+    (red_dot,) = ax.plot(
+        [], [], "o", color=RGB(main_color[0], main_color[1], main_color[2])
+    )
 
     # Create legend
     ax.legend()
