1fcbac8715b249a9dc6eb41e3ea9c72a2142c698
Ingrid Kristine Jacobsen <ingrid.kristine.jacobsen@gmail.com>
2024-03-05 11:16:14 +0100

    TST: Simple test before refactoring ABCs

---
+++ tests/ABC_model_for_testing.py
@@ -0,0 +1,56 @@
+import porepy as pp
+
+import numpy as np
+
+import sys
+
+sys.path.append("../")
+
+from models.absorbing_boundary_conditions import MomentumBalanceABC2
+
+
+class GeometryAndInitialCondition:
+    def initial_velocity(self, dofs: int) -> np.ndarray:
+        """Initial velocity values."""
+        sd = self.mdg.subdomains()[0]
+        t = self.time_manager.time
+
+        x = sd.cell_centers[0, :]
+        y = sd.cell_centers[1, :]
+
+        vals = np.zeros((self.nd, sd.num_cells))
+
+        theta = 1
+        lam = 0.125
+
+        common_part = theta * np.exp(
+            -np.pi**2 * ((x - 0.5) ** 2 + (y - 0.5) ** 2) / lam**2
+        )
+
+        vals[0] = common_part * (x - 0.5)
+
+        vals[1] = common_part * (y - 0.5)
+
+        return vals.ravel("F")
+
+    def nd_rect_domain(self, x, y) -> pp.Domain:
+        box: dict[str, pp.number] = {"xmin": 0, "xmax": x}
+
+        box.update({"ymin": 0, "ymax": y})
+
+        return pp.Domain(box)
+
+    def set_domain(self) -> None:
+        x = 1.0 / self.units.m
+        y = 1.0 / self.units.m
+        self._domain = self.nd_rect_domain(x, y)
+
+    def meshing_arguments(self) -> dict:
+        mesh_args: dict[str, float] = {"cell_size": 0.05 / self.units.m}
+        return mesh_args
+
+
+class MomentumBalanceABCForTesting(
+    GeometryAndInitialCondition,
+    MomentumBalanceABC2,
+): ...
