12445632cb993a46c902f02b10faf4059a7dd509
Ingrid Kristine Jacobsen <ingrid.kristine.jacobsen@gmail.com>
2025-02-18 11:47:37 +0000

    ENH(WIP): Interface to allow for "arbitrary" tensor

--- utils/anisotropy_mixins.py
+++ utils/anisotropy_mixins.py
@@ -1,16 +1,19 @@
-"""File for anisotropic stiffness tensor mixins. 
+"""File for anisotropic stiffness tensor mixins.
 
 Currently, the following tensors are found here:
     * Vertical transverse anisotropic stiffness tensor (for inner domain). Brute force
       version.
-    * Simple anisotropy: Only increase stiffness in one direction. 
+    * Simple anisotropy: Only increase stiffness in one direction.
     * Vertical transverse anisotropic stiffness tensor: Non-brute force version.
       Constructed generally enough to allow for some flexibility in further
       developments/enhancement of it."""
 
 import numpy as np
 import porepy as pp
-from utils import inner_domain_cells
+from utils.utility_functions import (
+    use_constraints_for_inner_domain_cells,
+    inner_domain_cells,
+)
 from utils.stiffness_tensors import StiffnessTensorInnerVTI
 
 
@@ -186,9 +189,9 @@ class SimpleAnisotropy:
             indices = inner_domain_cells(self=self, sd=subdomain, width=width)
             anisotropic_stiffness_values = self.construct_anisotropic_contribution()
             for cell_index in indices:
-                stiffness_tensor.values[
-                    :, :, cell_index
-                ] += anisotropic_stiffness_values
+                stiffness_tensor.values[:, :, cell_index] += (
+                    anisotropic_stiffness_values
+                )
 
         return stiffness_tensor
 
@@ -319,3 +322,222 @@ class InnerDomainVTIStiffnessTensorMixin:
         )
 
         return stiffness_tensor
+
+
+class MoreRobustTensorMixin:
+    def stiffness_tensor(self, subdomain: pp.Grid):
+        # Fetch inner domain indices such that we can distribute values of the material
+        # parameters in arrays according to cell numbers.
+        inner_cell_indices = use_constraints_for_inner_domain_cells(
+            self=self,
+            sd=subdomain,
+        )
+
+        # Preparing basis arrays for inner and outer domains
+        inner = np.zeros(subdomain.num_cells)
+        inner[inner_cell_indices] = 1
+
+        outer = np.ones(subdomain.num_cells)
+        outer = outer - inner
+
+        # Non-standard matrices
+        lmbda_mat = np.array(
+            [
+                [1, 0, 0, 0, 1, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 1, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 1, 0, 0, 0, 1],
+            ]
+        )
+
+        lambda_parallel_mat = np.array(
+            [
+                [1, 0, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ]
+        )
+
+        lambda_orthogonal_mat = np.array(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 1],
+            ]
+        )
+
+        mu_parallel_mat = np.array(
+            [
+                [2, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 2, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ]
+        )
+
+        mu_orthogonal_mat = np.array(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 1, 0],
+                [0, 0, 1, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 2],
+            ]
+        )
+        # Standard material values: These are assigned to the outer domain
+        lmbda = self.solid.lame_lambda * outer
+        mu = self.solid.shear_modulus * outer
+
+        # Anisotropy related values: These are assigned to the inner domain
+        anisotropy_constants = self.params["anisotropy_constants"]
+
+        mu_parallel = anisotropy_constants["mu_parallel"] * inner
+        mu_orthogonal = anisotropy_constants["mu_orthogonal"] * inner
+
+        volumetric_compr_lambda = (
+            anisotropy_constants["volumetric_compr_lambda"] * inner
+        )
+
+        lambda_parallel = anisotropy_constants["lambda_parallel"] * inner
+        lambda_orthogonal = anisotropy_constants["lambda_orthogonal"] * inner
+
+        # Finally a call to the stiffness tensor object itself
+        stiffness_tensor = TensorAllowingForCustomFields(
+            mu=mu,
+            lmbda=lmbda,
+            other_fields={
+                "mu_parallel": (mu_parallel_mat, mu_parallel),
+                "mu_orthogonal": (mu_orthogonal_mat, mu_orthogonal),
+                "volumetric_compr_lambda": (lmbda_mat, volumetric_compr_lambda),
+                "lambda_parallel": (lambda_parallel_mat, lambda_parallel),
+                "lambda_orthogonal": (lambda_orthogonal_mat, lambda_orthogonal),
+            },
+        )
+
+        return stiffness_tensor
+
+
+class TensorAllowingForCustomFields(pp.FourthOrderTensor):
+    def __init__(
+        self,
+        mu: np.ndarray,
+        lmbda: np.ndarray,
+        other_fields: dict[str, tuple[np.ndarray, np.ndarray]] | None = None,
+    ):
+        """Constructor for fourth order tensor on Lame-parameter form.
+
+        Implementation somewhat mirrors that found in the branch restrict_tensor (by
+        Eirik).
+        """
+
+        # Save lmbda and mu, can be useful to have in some cases
+        self.lmbda = lmbda
+        """Nc array of first Lamé parameter."""
+
+        self.mu = mu
+        """Nc array of shear modulus (second Lamé parameter)."""
+
+        # Basis for the contributions of mu, lmbda and phi is hard-coded
+        mu_mat = np.array(
+            [
+                [2, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 1, 0, 0],
+                [0, 1, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 2, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 1, 0],
+                [0, 0, 1, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 2],
+            ]
+        )
+        lmbda_mat = np.array(
+            [
+                [1, 0, 0, 0, 1, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 1, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 1, 0, 0, 0, 1],
+            ]
+        )
+
+        # Expand dimensions to prepare for cell-wise representation.
+        mu_mat = mu_mat[:, :, np.newaxis]
+        lmbda_mat = lmbda_mat[:, :, np.newaxis]
+
+        # List of constitutive parameters
+        self._constitutive_parameters = ["mu", "lmbda"]
+
+        c = mu_mat * mu + lmbda_mat * lmbda
+
+        # Store the other fields. This is needed for the copy method.
+        self._other_matrices = {}
+
+        for key, (mat, field) in other_fields.items():
+            c += mat[:, :, np.newaxis] * field
+            setattr(self, key, field)
+            self._other_matrices[key] = mat
+            self._constitutive_parameters.append(key)
+
+        self.values = c
+        """Values of the stiffness tensor as a (3^2, 3^2, Nc) array."""
+
+    def copy(self):
+        """Define a deep copy of the tensor.
+
+        Returns:
+            FourthOrderTensor: New tensor with identical fields, but separate arrays (in
+                the memory sense).
+
+        """
+        extra_params = {}
+        for key, mat in self._other_matrices.items():
+            extra_params[key] = (mat, getattr(self, key).copy())
+
+        C = TensorAllowingForCustomFields(
+            mu=self.mu.copy(), lmbda=self.lmbda.copy(), other_fields=extra_params
+        )
+        C.values = self.values.copy()
+        return C
+
+    @staticmethod
+    def constitutive_parameters(self):
+        return [
+            "mu",
+            "lmbda",
+            "mu_parallel",
+            "mu_orthogonal",
+            "lambda_parallel",
+            "lambda_orthogonal",
+            "volumetric_compr_lambda",
+        ]
