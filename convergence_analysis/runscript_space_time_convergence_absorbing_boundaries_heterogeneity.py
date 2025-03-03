8ab8b764e3d542c37d7b49c4618856393970ddc7
Ingrid Kristine Jacobsen <ingrid.kristine.jacobsen@gmail.com>
2025-03-03 15:31:03 +0000

    MISC: Adaptations done to runscript for all convergence analyses

--- convergence_analysis/runscript_space_time_convergence_absorbing_boundaries_heterogeneity.py
+++ convergence_analysis/runscript_space_time_convergence_absorbing_boundaries_heterogeneity.py
@@ -14,6 +14,7 @@ from convergence_analysis.convergence_analysis_models.model_convergence_ABC_hete
 
 # Prepare path for generated output files
 folder_name = "convergence_analysis_results"
+header = "num_cells, num_time_steps, displacement_error, traction_error\n"
 script_dir = os.path.dirname(os.path.abspath(__file__))
 output_dir = os.path.join(script_dir, folder_name)
 os.makedirs(output_dir, exist_ok=True)
@@ -26,47 +27,77 @@ class ConvergenceAnalysisHeterogeneity(ABCModelHeterogeneous):
         return mesh_args
 
 
-heterogeneity_factors = [1, 2, 3, 4, 5, 6]
-for factor in heterogeneity_factors:
-    filename = f"displacement_and_traction_errors_heterogeneity_{str(factor)}.txt"
-    filename = os.path.join(output_dir, filename)
+heterogeneity_factors = [
+    # Homogeneous case
+    1,
+    # Heterogeneous case
+    1 / 2**5,
+    # Heterogeneous case
+    1 / 2**7,
+]
+anisotropy_factors_mu_lambda = [
+    # Isotropic case
+    (1, 0),
+    # Anisotropic case
+    (1, 0.001),
+    # Anisotropic case
+    (1, 0.000001),
+]
 
-    refinements = np.arange(0, 7)
-    for refinement_coefficient in refinements:
-        if refinement_coefficient == 0:
-            with open(filename, "w") as file:
-                file.write(
-                    "num_cells, num_time_steps, displacement_error, traction_error\n"
-                )
-        tf = 15.0
-        time_steps = 15 * (2**refinement_coefficient)
-        dt = tf / time_steps
+for heterogeneity_factor_index in range(0, len(heterogeneity_factors)):
+    h_h = heterogeneity_factors[heterogeneity_factor_index]
+    for (
+        h_mu,
+        h_lambda,
+    ) in anisotropy_factors_mu_lambda:
+        filename = f"errors_heterogeneity_{str(heterogeneity_factor_index)}_mu_{str(h_mu)}_lam_{str(h_lambda)}.txt"
 
-        time_manager = pp.TimeManager(
-            schedule=[0.0, tf],
-            dt_init=dt,
-            constant_dt=True,
-        )
+        filename = os.path.join(output_dir, filename)
 
-        lame_lambda, shear_modulus = 0.01, 0.01
+        refinements = np.arange(1, 5)
+        for refinement_coefficient in refinements:
+            if refinement_coefficient == 1:
+                with open(filename, "w") as file:
+                    file.write(header)
+            tf = 15.0
+            time_steps = 15 * (2**refinement_coefficient)
+            dt = tf / time_steps
 
-        solid_constants = pp.SolidConstants(
-            lame_lambda=lame_lambda, shear_modulus=shear_modulus
-        )
-        material_constants = {"solid": solid_constants}
+            time_manager = pp.TimeManager(
+                schedule=[0.0, tf],
+                dt_init=dt,
+                constant_dt=True,
+            )
 
-        params = {
-            "time_manager": time_manager,
-            "grid_type": "simplex",
-            "progressbars": True,
-            "folder_name": "heterogeneity",
-            "heterogeneity_factor": 1 / 2**factor,
-            "heterogeneity_location": 0.5,
-            "material_constants": material_constants,
-            "meshing_kwargs": {"constraints": [0, 1, 2, 3]},
-        }
+            lame_lambda, shear_modulus = 0.01, 0.01
 
-        model = ConvergenceAnalysisHeterogeneity(params)
-        model.refinement = refinement_coefficient
-        model.filename_path = filename
-        rlm.run_linear_model(model, params)
+            solid_constants = pp.SolidConstants(
+                lame_lambda=lame_lambda, shear_modulus=shear_modulus
+            )
+            material_constants = {"solid": solid_constants}
+
+            anisotropy_constants = {
+                "mu_parallel": shear_modulus,
+                "mu_orthogonal": shear_modulus * h_mu,
+                "lambda_parallel": 0.0,
+                "lambda_orthogonal": lame_lambda * h_lambda,
+                "volumetric_compr_lambda": lame_lambda,
+            }
+
+            params = {
+                "time_manager": time_manager,
+                "grid_type": "simplex",
+                "progressbars": True,
+                "folder_name": "heterogeneity",
+                "heterogeneity_factor": h_h,
+                "heterogeneity_location": 0.5,
+                "material_constants": material_constants,
+                "anisotropy_constants": anisotropy_constants,
+                "symmetry_axis": [0, 1, 0],
+                "meshing_kwargs": {"constraints": [0, 1, 2, 3]},
+            }
+
+            model = ConvergenceAnalysisHeterogeneity(params)
+            model.refinement = refinement_coefficient
+            model.filename_path = filename
+            rlm.run_linear_model(model, params)
